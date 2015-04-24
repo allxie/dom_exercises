8928da8530e8324723cc11d50cc262de5fa025c3
Allxie <allxiecleary@gmail.com>
2015-04-24 10:22:45 -0700

    completed dom exercises

--- collections/collections.js
+++ collections/collections.js
@@ -2,17 +2,34 @@
 // Part 1
 //
 
+var setBorder = function(){
+	var lis = document.getElementsByTagName("li");
+
+	for (var x = 0; x < lis.length; x++){
+		lis[x].style.borderBottom = "5px solid black";
+	}
+
+}
 
 
 
 //
 // Part 2
 //
-
-
-
+var gonnaBeRed = function (){
+	var dT = document.getElementsByClassName("datatype");
+	for (var x = 0; x < dT.length; x++){
+		dT[x].style.color = "red";
+	}
+}
 
 //
 // Part 3
 //
 
+var gonnaBeBlue = function(){
+	var classes = document.querySelectorAll(".function");
+	for (var x = 0; x < classes.length; x++){
+		classes[x].style.color= "blue";
+	}
+}
--- events/events.js
+++ events/events.js
@@ -1,1 +1,68 @@
 // Your code here
+window.onload = function myScript(){
+// 1.) ALERT
+var showPopUp = function () {
+  alert("the button was clicked");
+}
+
+var button = document.getElementById("clicker");
+button.addEventListener("click", showPopUp);
+
+// 2.) Hover over change background
+
+var hoverBackground = function() {
+	mouser.style.background= "yellow";
+}
+var noHoverBackground = function() {
+	mouser.style.background= "green";
+}
+
+var square = document.getElementById("mouser");
+square.addEventListener("mouseover", hoverBackground);
+square.addEventListener("mouseout", noHoverBackground);
+
+
+// 3.) Listen for submit, alert the text from the field
+
+//We have a submit button--let's call it submit
+var submitT = document.getElementById("submit");
+//when you click it, do alertText
+submitT.onclick = stopDefAction;
+submitT.onclick = alertText;
+//here's what you're going to do when we click...
+var alertText = function() {
+//let's call the stuff in user input 'field.''
+	var field = document.getElementById("userInput");
+	//say the thing when we click.
+	var userWords = field.value;
+	alert(userWords);
+};
+function stopDefAction(event) {
+    event.preventDefault();
+};
+// 5.) //figure out which veggie was clicked and alert it. 
+
+var veg = document.getElementById('vegetables');
+// veg.onclick = alert("WTF");
+
+var alertVeg = function() {
+	var x = event.target;
+	alert(x.innerHTML);
+}
+veg.addEventListener("click", alertVeg);
+
+
+// veg.addEventListener('click', function(){
+// 	var x = event.target;
+// 	alert(x.innerHTML);
+// });
+
+
+
+};
+
+
+
+
+
+
